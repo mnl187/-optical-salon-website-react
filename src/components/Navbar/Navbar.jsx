4346c0d74604a3963bfa8f0b8d2e5afa35d6edba
mnl187 <mnl187@gmail.com>
2023-02-21 22:56:56 +0100

    dodanie navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { GiHamburgerMenu} from "react-icons/all";
+import { MdOutlineBusiness } from "react-icons/all";
+import images from "../../constants/images";
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo"/>
+    </div>
+  </nav>
 );
 
 export default Navbar;
