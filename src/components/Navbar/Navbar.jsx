108e6ec01dc22e4b88e8576075c64375983a15c2
mnl187 <mnl187@gmail.com>
2023-02-22 22:21:28 +0100

    dodaie elementtu  umawiania wizty w navbar oraz stylu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { GiHamburgerMenu} from "react-icons/all";
-import { MdOutlineBusiness } from "react-icons/all";
+// import { GiHamburgerMenu} from "react-icons/all";
+// import { MdOutlineBusiness } from "react-icons/all";
 import images from "../../constants/images";
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-        <img src={images.opticalSalonLogo} alt="optical salon logo"/>
-    </div>
-      <ul className="app__navbar-links">
-          <li className="p__opensans"><a href="#home">Home</a></li>
-          <li className="p__opensans"><a href="#about">O nas</a></li>
-          <li className="p__opensans"><a href="#contact">Kontakt</a></li>
-      </ul>
-  </nav>
+    <nav className="app__navbar">
+        <div className="app__navbar-logo">
+            <img src={images.opticalSalonLogo} alt="optical salon logo"/>
+        </div>
+        <ul className="app__navbar-links">
+            <li className="p__opensans"><a href="#home">Home</a></li>
+            <li className="p__opensans"><a href="#about">O nas</a></li>
+            <li className="p__opensans"><a href="#contact">Kontakt</a></li>
+        </ul>
+        <div className="app__navbar-visit">
+            <a href="/" className="p__opensans">Umów wizytę u lekarza</a>
+        </div>
+    </nav>
 );
 
 export default Navbar;
