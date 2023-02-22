76433a7038fcba911d95f295968f5ff6a48092eb
mnl187 <mnl187@gmail.com>
2023-02-22 22:58:42 +0100

    dodanie menu i stylów dla mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { GiHamburgerMenu} from "react-icons/all";
-// import { MdOutlineBusiness } from "react-icons/all";
+import {GiHamburgerMenu} from "react-icons/gi";
+import {MdOutlineBusiness} from "react-icons/md";
 import images from "../../constants/images";
 import './Navbar.css';
 
@@ -16,6 +16,20 @@ const Navbar = () => (
         </ul>
         <div className="app__navbar-visit">
             <a href="/" className="p__opensans">Umów wizytę u lekarza</a>
+        </div>
+        <div className="app__navbar-smallscreen">
+            <GiHamburgerMenu color="fff" fontSize={27} onClick={() => {
+            }}/>
+
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                <MdOutlineBusiness fontSize={27} className="overlay__close" onClick={() => {
+                }}/>
+                <ul className="app__navbar-smallscreen-links">
+                    <li className="p__opensans"><a href="#home">Home</a></li>
+                    <li className="p__opensans"><a href="#about">O nas</a></li>
+                    <li className="p__opensans"><a href="#contact">Kontakt</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 );
