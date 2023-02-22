e3046c68ee829a16f2b52eb4b1206a2f6a9836c6
mnl187 <mnl187@gmail.com>
2023-02-22 23:14:35 +0100

    dodanie funckji dla wyswietlania menu w przypadku malego ui duzego ekranu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GiHamburgerMenu} from "react-icons/gi";
 import {MdOutlineBusiness} from "react-icons/md";
 import images from "../../constants/images";
 import './Navbar.css';
 
-const Navbar = () => (
-    <nav className="app__navbar">
-        <div className="app__navbar-logo">
-            <img src={images.opticalSalonLogo} alt="optical salon logo"/>
-        </div>
-        <ul className="app__navbar-links">
-            <li className="p__opensans"><a href="#home">Home</a></li>
-            <li className="p__opensans"><a href="#about">O nas</a></li>
-            <li className="p__opensans"><a href="#contact">Kontakt</a></li>
-        </ul>
-        <div className="app__navbar-visit">
-            <a href="/" className="p__opensans">Umów wizytę u lekarza</a>
-        </div>
-        <div className="app__navbar-smallscreen">
-            <GiHamburgerMenu color="fff" fontSize={27} onClick={() => {
-            }}/>
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
 
-            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-                <MdOutlineBusiness fontSize={27} className="overlay__close" onClick={() => {
-                }}/>
-                <ul className="app__navbar-smallscreen-links">
-                    <li className="p__opensans"><a href="#home">Home</a></li>
-                    <li className="p__opensans"><a href="#about">O nas</a></li>
-                    <li className="p__opensans"><a href="#contact">Kontakt</a></li>
-                </ul>
+    return (
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.opticalSalonLogo} alt="optical salon logo"/>
             </div>
-        </div>
-    </nav>
-);
+            <ul className="app__navbar-links">
+                <li className="p__opensans"><a href="#home">Home</a></li>
+                <li className="p__opensans"><a href="#about">O nas</a></li>
+                <li className="p__opensans"><a href="#contact">Kontakt</a></li>
+            </ul>
+            <div className="app__navbar-visit">
+                <a href="/" className="p__opensans">Umów wizytę u lekarza</a>
+            </div>
+            <div className="app__navbar-smallscreen">
+                <GiHamburgerMenu color="fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+
+                {toggleMenu && (
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                        <MdOutlineBusiness fontSize={27} className="overlay__close"
+                                           onClick={() => setToggleMenu(false)}/>
+                        <ul className="app__navbar-smallscreen-links">
+                            <li className="p__opensans"><a href="#home">Home</a></li>
+                            <li className="p__opensans"><a href="#about">O nas</a></li>
+                            <li className="p__opensans"><a href="#contact">Kontakt</a></li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </nav>
+    )
+};
 
 export default Navbar;
