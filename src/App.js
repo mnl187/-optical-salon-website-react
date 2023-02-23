d157312fefeae6a21911add625f2fff3b039d17d
mnl187 <mnl187@gmail.com>
2023-02-23 20:50:52 +0100

    refaktoryzacja importów App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
-
-import { AboutUs, FindUs, Footer, Gallery, Header, Intro, Services } from './container';
-import { Navbar } from './components';
+import React from 'react'
+import AboutUs from './container/AboutUs/AboutUs';
+import FindUs from './container/Findus/FindUs';
+import Footer from './container/Footer/Footer';
+import Gallery from './container/Gallery/Gallery';
+import Header from './container/Header/Header';
+import Intro from './container/Intro/Intro';
+import Services from './container/Services/Services';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 const App = () => (
