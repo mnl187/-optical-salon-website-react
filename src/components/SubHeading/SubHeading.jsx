0ac2896bc2bcff08ce09a0293cb7e628c6d51b2d
mnl187 <mnl187@gmail.com>
2023-02-23 23:23:26 +0100

    dodanie SubHeading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {women01} from "../../constants/images";
 
 const SubHeading = () => (
-  <div>
-    SubHeading
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">Tytuł</p>
+      <img src={women01} alt="women image" className="women01__img"/>
   </div>
 );
 
