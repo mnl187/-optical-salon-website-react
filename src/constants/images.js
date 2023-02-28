6b4c24e331bd0cf399c2f98073a37faa5abd03e1
mnl187 <mnl187@gmail.com>
2023-02-28 22:26:44 +0100

    dodanie kolejnej sekcji w About Us, podmiana obrazka tła

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,7 @@
 import logo from '../assets/logo.png';
 import women01 from '../assets/ai-blond-women-sunglasses01.png';
 import flat_black_M from '../assets/flat_black_M.png';
+import ai_women_sunglasses01 from '../assets/ai_women_sunglasses01.png';
 
 
-export {logo, women01, flat_black_M };
+export {logo, women01, flat_black_M, ai_women_sunglasses01 };
